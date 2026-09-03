e3ed07300fc8606832a82a5ba52525e7295a269e
vaishnavig8870-collab <vaishnavig8870@gmail.com>
2026-09-03 23:55:46 +0530

    server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get("/", (req, res) => {
     res.send("Call Assistant Backend is Running!");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
